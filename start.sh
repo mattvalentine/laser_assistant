9d149f1711425b6d6112d7afe3758042ced0f339
Matt Valentine <valentine@Matts-MBP.lan>
2020-08-05 13:29:40 -0400

    rework to fix python 2 vs 3 glitch on glitch

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # git clone https://github.com/mathandy/svgpathtools.git
 # python svgpathtools/setup.py install
 
-# python laser_flask.py
+python3 laser_flask.py
 
-export FLASK_APP=laser_flask.py
-flask run
+# export FLASK_APP=laser_flask.py
+# flask run
